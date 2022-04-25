cdc426376725a5f01e2b8ca05f6b6ff81b8e58c4
rahmiddinasiydinov <rahmiddinasiydinov@gmail.com>
2022-04-25 14:12:37 +0500

    fix: netlify toml

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/*" element={<Notfound />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
       <Footer />
