a7e6fd6a8e09c64a9aee53d30437b6df36dd98d0
rahmiddinasiydinov <rahmiddinasiydinov@gmail.com>
2022-04-27 14:58:04 +0500

    feat: post new appliaction

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,16 +1,64 @@
 import './Project.scss';
 import play from '../../Assets/Images/play.png';
 import round from '../../Assets/Images/round.png';
-import { Link } from 'react-router-dom';
+import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import React, { useState } from 'react';
 function Project() {
-    
-    return (
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+      setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+      if (reason === "clickaway") {
+        return;
+      }
+
+      setOpen(false);
+    }
+     
+  function sendApplication(e) {
+    e.preventDefault();
+    const { name, desc, title, email } = e.target.elements;
+    console.log(name.value, desc.value, title.value, email.value);
+    fetch("https://nargiz-back.herokuapp.com/application", {
+      method: "POST", 
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        name: name.value,
+        desc: desc.value, 
+        email: email.value, 
+        title:title.value
+      })
+    }).then(res => res.json())
+      .then(data => {
+        if (data?.status === 200) {
+          handleClick();
+          console.log(data);
+        }
+      });
+  }
+  return (
+    <Stack spacing={2} sx={{ width: "100%" }}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Sent
+        </Alert>
+      </Snackbar>
       <section className="project">
         <div className="container">
           <div className="project__wrapper">
             <div className="project__content">
               <h2 className="project__title">
-                Have a project! <br/> Let’s discuss{" "}
+                Have a project! <br /> Let’s discuss{" "}
               </h2>
               <p className="project__text">
                 Thank you for getting in touch!
@@ -31,7 +79,7 @@ function Project() {
               <p className="project__form--text">
                 To contact us leave your number below
               </p>
-              <form className="project__appeal">
+              <form className="project__appeal" onSubmit={sendApplication}>
                 <div className="project__input--wrapper">
                   <input
                     className="project__input name__input"
@@ -57,12 +105,18 @@ function Project() {
                   name="desc"
                   className="project__textarea"
                 ></textarea>
-                <Link to='/products' className='project__submit'>SENT MESSAGE</Link>
+                <button
+                  type="submit"
+                  className="project__submit"
+                >
+                  SENT MESSAGE
+                </button>
               </form>
             </div>
           </div>
         </div>
       </section>
-    );
+    </Stack>
+  );
 }
 export default Project
